26a830f4fff7d442621e0203f2839be043d22827
RazvanBugoi <razvanbugoi@gmail.com>
2020-08-31 21:52:32 +0100

    changed the logic of my for loop and made it simpler by using one single class for all my boxes instead of individuals ids

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
 let colourCode = document.getElementById('colour-code');
+let boxes = document.getElementById('allBoxes').getElementsByClassName('box');
+let winnerNumber;
+let colours = [];
 
-
-let boxes = document.getElementsByClassName('allBoxes');
-let winnerBtn;
-
-for (let i = 0; i < boxes[0].childElementCount; i += 1) {
-    console.log(boxes[0].children[i]);
-    boxes[0].children[i].style.background = `rgb(${getColourCode()},${getColourCode()},${getColourCode()})`;
-    boxes[0].children[i].addEventListener('click', function (event) {
-        if (this.style.background == winnerBtn.style.background) {
+for (let i = 0; i < boxes.length; i += 1) {
+    console.log(boxes[i]);
+    boxes[i].style.background = `rgb(${getColourCode()},${getColourCode()},${getColourCode()})`;
+    boxes[i].addEventListener('click', function (event) {
+        if (this.style.background == winnerNumber.style.background) {
             alert('you won!');
         } else {
             alert('Wrong box :( Try again.')
@@ -18,19 +17,14 @@ for (let i = 0; i < boxes[0].childElementCount; i += 1) {
 
 
 function winnerBox() {
-    let winnerNum = Math.round(Math.random() * 6);
-    if (winnerNum == 0) { winnerNum = 1 };
-    const winnerNumber = document.getElementById(`box${winnerNum}`);
-    console.log(winnerNumber);
-    winnerBtn = winnerNumber;
+    let winnerNum = Math.round(Math.random() * 5);
+    winnerNumber = boxes[winnerNum];
+    console.log(winnerNum);
 }
 
 winnerBox()
 
-colourCode.innerHTML = winnerBtn.style.background;
-
-
-let colours = [];
+colourCode.innerHTML = winnerNumber.style.background;
 
 for (let i = 0; i <= 255; i += 1) {
     colours.push(i);
