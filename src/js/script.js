00be29a7ed07e3f153d8cd4431684233bc953a59
RazvanBugoi <razvanbugoi@gmail.com>
2020-08-31 14:14:24 +0100

    deleted old code

--- src/js/script.js
+++ src/js/script.js
@@ -28,33 +28,6 @@ function winnerBox() {
 winnerBox()
 
 colourCode.innerHTML = winnerBtn.style.background;
-// let box1 = document.getElementById('box1');
-// let box2 = document.getElementById('box2');
-// let box3 = document.getElementById('box3');
-// let box4 = document.getElementById('box4');
-// let box5 = document.getElementById('box5');
-// let box6 = document.getElementById('box6');
-
-// box1.style.background = `${colourCode.value}`;
-// box2.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-// box3.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-// box4.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-// box5.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-// box6.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-
-// let reset = document.getElementById('reset');
-// reset.onclick = changeColours;
-
-// function changeColours() {
-//     colourCode.value = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-//     box1.style.background = `${colourCode.value}`;
-//     box2.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-//     box3.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-//     box4.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-//     box5.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-//     box6.style.background = `rgb(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`;
-// }
-
 
 
 let colours = [];
@@ -67,20 +40,5 @@ function getColourCode() {
     return Math.floor(Math.random() * 255);
 }
 
-// box1.onclick = testTheBox;
-// box2.onclick = testTheBox;
-// box3.onclick = testTheBox;
-// box4.onclick = testTheBox;
-// box5.onclick = testTheBox;
-// box6.onclick = testTheBox;
-
-// function testTheBox(this) {
-//     if this.syle.background == `${colourCode.value}` {
-//         alert('This is the right colour code, you won!')
-//     } else {
-//         alert('Ups, this is not the same colour, try again.')
-//         changeColours();
-//     }
-// }
 
 
